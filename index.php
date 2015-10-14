7f438b0f67937e04ecfc9c0050034d463a3f3579
Bradley Brown <tall.white.ninja@gmail.com>
2015-10-14 15:48:59 -0600

    Added most of the information about the persona, and started a use case

--- index.php
+++ index.php
@@ -5,6 +5,26 @@
 		<title>Data Design Project</title>
 	</head>
 	<body>
-		<p>test</p>
+		<h2>Persona</h2>
+		<p>The user of the commenting feature will almost certainly be a fan of the sport of soccer - and of Major League Soccer in particular - and would likely have some degree of knowledge in the sport.
+			They would likely be American (either by birth or current residence), as MLS is an American sports league and fans from other nations would typically follow either those nations' leagues, or larger ones.
+			American soccer fans tend to be teens and young adults (millenials) and often have roots in a country where the sport is more widespread (such as immigrants or first generation Americans).
+			Younger generations, in general, are typically more comfortable around technology, and heavily use mobile devices such as smart phones in particular.
+			Someone who comments on the site would be looking to discuss the sport (and the contents of a given article in particular) with other fans, and most likely fans of the same team.
+		</p>
+		<h2>Use Case: Commenting on an Article</h2>
+		<ul>
+			<li>User goes to MLSSoccer.com</li>
+			<li>User selects "Login"</li>
+			<li>The site prompts a user with a window to either log in to their profile, or create a new one</li>
+			<li>The user either:
+				<ul>
+					<li>Logs in to their existing profile with a username and password</li>
+					<li>Creates a new profile with a new username and new password</li>
+				</ul>
+			</li>
+			<li>The site uses this information to log in the user, creating a profile if necessary</li>
+			<li>The user selects the article </li>
+		</ul>
 	</body>
 </html>
