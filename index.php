5deb46fce30dd8c87f7e57983ebd9b988bf10c98
Bradley Brown <tall.white.ninja@gmail.com>
2015-10-14 16:20:10 -0600

    Made more changes to both persona and use case

--- index.php
+++ index.php
@@ -10,13 +10,17 @@
 			They would likely be American (either by birth or current residence), as MLS is an American league.
 			American soccer fans tend to be teens and young adults (millenials) and often have roots in a country where the sport is more widespread (such as immigrants or first generation Americans).
 			Younger generations, in general, are typically more comfortable around technology, and heavily use mobile devices such as smart phones in particular.
-			Someone who comments on the site would be looking to discuss the sport (and the contents of a given article in particular) with other fans, and most likely fans of the same team.
+			Someone who comments on the site would be looking to discuss the sport (and the contents of a given article) with other fans.
+			They are most likely to be drawn into reading and commenting articles about their favorite team, so most other commentators they interact with will be fans of the team, as well.
 		</p>
-		<h2>Use Case: Reading an article, and posting a comment in response</h2>
+
+		<p>The persona is an American under the age of 35, likely with immigrant roots, who enjoys soccer (and one team in particular) and wishes to read more about their favorite team and discuss things with other fans of said team.
+		They are very comfortable mobile users.</p>
+		<h2>Use Case: Reading an article about a team, and posting a comment in response</h2>
 		<ul>
 			<li>User goes to MLSSoccer.com</li>
 			<li>The user scrolls through the list of current articles on the site</li>
-			<li>The user clicks on the article they wish to read and comment on</li>
+			<li>The user clicks on the article they wish to read and potentially comment on</li>
 			<li>The site retrieves and displays the article</li>
 			<li>The user reads the article, and decides to comment</li>
 			<li>The user either scrolls to the comment section, or pushes the comment button to jump there</li>
@@ -29,8 +33,10 @@
 				</ul>
 			</li>
 			<li>The site uses this information to log in the user, creating a new user profile if necessary</li>
-			<li>The user enters the message they wish to post to the comment board, and selects the "Post" button</li>
+			<li>The user enters the message they wish to post to the comment board into the given field</li>
+			<li>The user selects the "Post" button</li>
 			<li>The site posts the comment</li>
 		</ul>
+		<h2></h2>
 	</body>
 </html>
