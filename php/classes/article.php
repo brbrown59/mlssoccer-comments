e17b73d5c5b54a69dbb03d391a26ecc719646baa
Bradley Brown <tall.white.ninja@gmail.com>
2015-10-21 15:37:14 -0600

    Created file for the article class

---
+++ php/classes/article.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * An article on MLSsoccer.com
+ *
+ * This is the class for any of the articles on MLSSoccer.com
+ * It contains the text of the article itself, in addition to the author and title
+ *
+ * @author Bradley Brown <tall.white.ninja@gmail.com>
+ */
+
+class Article{
+
+}
