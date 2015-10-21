dd75621ef9296152e2d8ce7afd2a06a254b8aa3b
Bradley Brown <tall.white.ninja@gmail.com>
2015-10-21 15:44:42 -0600

    added state variables and one accessor to the aritcle class

--- php/classes/article.php
+++ php/classes/article.php
@@ -10,5 +10,33 @@
  */
 
 class Article{
+	/**
+	 * id for the article; this is the primary key
+	 * @var int $articleId
+	 */
+	private $articleId;
+	/**
+	 * the author of the article
+	 * @var string $author
+	 */
+	private $author;
+	/**
+	 * the actual text of the article itself
+	 * @var string $text
+	 */
+	private $text;
+	/**
+	 * the title of the article
+	 * @var string $title
+	 */
+	private $title;
 
+	/*
+	 * accessor method for the articleId
+	 *
+	 * @return mixed value of article Id
+	 */
+	public function getArticleId(){
+		return ($this->articleId);
+	}
 }
