d9181af9a42e85ce41acd43fd519cb17a4750cd7
Bradley Brown <tall.white.ninja@gmail.com>
2015-10-21 13:52:49 -0600

    Created the file containing the mlsUser class, added the initial docblock and the state variables

---
+++ php/classes/mlsUser.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ *
+ * A user profile for the comments of MLSsoccer.com
+ *
+ * This is the basic profile for any user of MLSsoccer.com's comment section
+ * It contains the basic public details viewable to other commentors
+ *
+ * @author Bradley Brown <tall.white.ninja@gmail.com>
+ */
+class mlsUser {
+	/**
+	 *  id for this user; this is the primary key
+	 * @var int $userId
+	 */
+	private $userId;
+	/**
+	 *  the username of this particular user
+	 * @var string $username
+	 */
+	private $username;
+	/**
+	 *  the photographic avatar of the user, in URL form
+	 * @var string $avatar
+	 */
+	private $avatar;
+
+	/*
+	 * accessor method for userId
+	 *
+	 */
+}
