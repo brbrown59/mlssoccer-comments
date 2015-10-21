d66746872aa7fede3ab10b5d623949f99aae449d
Bradley Brown <tall.white.ninja@gmail.com>
2015-10-21 14:04:42 -0600

    Created all of the accessor methods, and the mutator method for userId

--- php/classes/mlsUser.php
+++ php/classes/mlsUser.php
@@ -29,5 +29,57 @@ class mlsUser {
 	/*
 	 * accessor method for userId
 	 *
+	 * @return mixed value of user Id
 	 */
+	public function getUserId(){
+		return($this->userId);
+	}
+
+	/*
+	 * mutator method for user Id
+	 * @param mixed $newUserId value of user ID
+	 * @throws InvalidArgumentException if $newUserId is not an integer
+	 * @throws RangeException if $newUserId is not positive
+	 */
+	public function setUserId($newUserId){
+		//base case: it is a new user, and there is no mySQL ID assigned as of yet
+		if($newUserId === null){
+			$this->userId = null;
+			return;
+		}
+
+		//verify that the ID is a valid integer
+		$newUserId = filter_var($newUserId, FILTER_VALIDATE_INT);
+		if($newUserId === false){
+			throw(new InvalidArgumentException("User ID is not a valid integer."));
+		}
+
+		//now, verify that the ID number is positive
+		if($newUserId <= 0){
+			throw(new RangeException("User ID is not positive"));
+		}
+
+		//convert the value to integer, and stroe
+		$this->userId = $newUserId;
+	}
+
+	/*
+	 * accessor method for username
+	 *
+	 * @return string value of user Id
+	 */
+	public function getUsername(){
+		return($this->username);
+	}
+
+	/*
+	 * accessor method for avatar
+	 *
+	 * @return string value of avatar
+	 */
+	public function getAvatar(){
+		return($this->avatar);
+	}
+
+
 }
