72d2c9b30e5689f711f1d1b79b2ce4a74cde57ba
Bradley Brown <tall.white.ninja@gmail.com>
2015-10-22 08:30:30 -0600

    defined the state variables of the comment

--- php/classes/comment.php
+++ php/classes/comment.php
@@ -1,4 +1,5 @@
 <?php
+require_once(dirname(dirname(__DIR__)) . "/lib/php/date-utils.php");
 
 /**
  * A comment on MLSsoccer.com
@@ -10,5 +11,29 @@
  *@author Bradley Brown <tall.white.ninja@gmail.com>
  **/
 class Comment{
-
+	/**
+ 	* id for the comment; this is the primary key
+ 	* @var int $commentId
+ 	**/
+	private $commentId;
+	/**
+	 * id for the article to which this comment is posted; this is a foreign key
+	 * @var int $articleId
+	 **/
+	private $articleId;
+	/**
+	 * id for the user who posted this comment; this is a foreign key
+	 * @var int userId
+	 **/
+	private $userId;
+	/**
+	 * time at which this article was posted, in a DateTime object
+	 * @var DateTime $time
+	 */
+	private $time;
+	/**
+	 * text of the posted comment itself
+	 * @var string $text
+	 **/
+	private $text;
 }
